2679dee977218106c47fa98d19794692d004df36
Camilo <camiloconet@gmail.com>
2024-12-27 20:30:05 -0700

    Update ReceiptController.java

--- Market-Management/Market-System/microservice-receipt/src/main/java/com/microservice/receipt/Controller/ReceiptController.java
+++ Market-Management/Market-System/microservice-receipt/src/main/java/com/microservice/receipt/Controller/ReceiptController.java
@@ -1,11 +1,11 @@
 package com.microservice.receipt.Controller;
 
+import com.microservice.receipt.Entity.Receipt;
 import com.microservice.receipt.Service.ServiceInterfaceReceipt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/receipt")
@@ -15,9 +15,16 @@ public class ReceiptController {
     private ServiceInterfaceReceipt serviceInterfaceReceipt;
 
     @PostMapping("/purchase")
-    public ResponseEntity purchaseProducts(){
-        
-        return ResponseEntity.ok().build();
+    public ResponseEntity purchaseProducts(@RequestBody Receipt receipt) {
+        serviceInterfaceReceipt.createReceipt(receipt);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getReceipt(@PathVariable("id") Long id) {
+        serviceInterfaceReceipt.getReceipt(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+
     }
 
 
